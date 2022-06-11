3508ce1ed81381522e7804b86871b9ead5a387e9
Swamy <thudumswamy786@gmail.com>
2022-06-11 22:25:23 +0530

    added tool bar

--- src/app/common/shared/shared.module.ts
+++ src/app/common/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { ToolBarComponent } from '../components/tool-bar/tool-bar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ToolBarComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
-  ]
+  ],
+  exports:[ToolBarComponent]
 })
 export class SharedModule { }
