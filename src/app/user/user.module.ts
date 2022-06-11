d69a535b4a955ae1f7dd8ff6b05468fe12c731a9
Swamy <thudumswamy786@gmail.com>
2022-06-11 12:40:56 +0530

    added login component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { PasswordComponent } from './password/password.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    PasswordComponent
+    PasswordComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
