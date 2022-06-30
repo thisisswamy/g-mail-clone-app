3d0d30794e8ab4e944bed98dd686a2d47be3a446
Swamy <thudumswamy786@gmail.com>
2022-06-30 10:50:12 +0530

    changed file name

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/g-mail-app'));
+app.use(express.static(__dirname + '/dist/g-mail-clone-app'));
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/g-mail-app/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/g-mail-clone-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
